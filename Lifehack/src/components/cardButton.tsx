b21a9bf7c3dcb78e7f04d4f95874fe3b4d63b663
Anna Odintsova <Klio7@mail.ru>
2025-09-29 11:59:33 +0300

    feat: change color, background color and color of the border for button in hover variant

--- Lifehack/src/components/cardButton.tsx
+++ Lifehack/src/components/cardButton.tsx
@@ -17,6 +17,7 @@ function CardButton({ buttonTitle, variant, bg, onClick }: ButtonProps) {
       fontSize="22px"
       fontWeight="400"
       onClick={onClick}
+      _hover={{ bg: "gray.500", color: "white", borderColor: "gray.500" }}
     >
       {buttonTitle}
     </Button>
